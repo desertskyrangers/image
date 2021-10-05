3b2facd1136aba6c84396ae04bc30dc1924e2935
msoderquist <msoderquist@mastercontrol.com>
2021-10-05 12:31:36 -0600

    New color theme

--- src/main/java/com/desertskyrangers/DesertSkyRangersIcon.java
+++ src/main/java/com/desertskyrangers/DesertSkyRangersIcon.java
@@ -11,9 +11,12 @@ public class DesertSkyRangersIcon extends SvgIcon {
 
 	private final boolean renderPlane;
 
-	private static final Color[] DESERT = new Color[]{ Color.web( "#b37c52" ), Color.web( "#4D4033" ), Color.web( "#6A806F" ) };
+	// Color theme is: sun, plane, mountains
+	private static final Color[] ARIZONA = new Color[]{ Color.web( "#b37c52" ), Color.web( "#4D4033" ), Color.web( "#6A806F" ) };
 
-	private final Color[] theme = DESERT;
+	private static final Color[] UTAH = new Color[]{ Color.web( "#b37c52" ), Color.web( "#403830" ), Color.web( "#786F52" ) };
+
+	private final Color[] theme = UTAH;
 
 	public DesertSkyRangersIcon() {
 		this( true );
@@ -32,18 +35,13 @@ public class DesertSkyRangersIcon extends SvgIcon {
 	protected void doRender2() {
 		String sun = circle( 16, 16, 15 );
 
-		//String mountain = "M2,30 L2,26 L8,20 L10,22 L16,16 L22,22 L24,20 L30,26 L30,30 Z";
-		String smallMountain = "M1,31 L1,27 L9,23 C10,22.5 10.75,22.25 11.5,22.25 C12.25,22.25 13,22.5 14,23 C18,25 26,29 30,29 L31,29 L31,31 Z";
-		String bigMountain = "M15,22.5 L18,21 C19,20.5 19.75,20.25 20.5,20.25 C21.25,20.25 22,20.5 23,21 L31,25 L31,28 L30,28 C26,28 19,24.5 15,22.5 Z";
-		String curvedMountain = smallMountain + bigMountain;
+		String foreMountain = "M1,31 L1,27 L9,23 C10,22.5 10.75,22.25 11.5,22.25 C12.25,22.25 13,22.5 14,23 C18,25 26,29 30,29 L31,29 L31,31 Z";
+		String backMountain = "M15,22.5 L18,21 C19,20.5 19.75,20.25 20.5,20.25 C21.25,20.25 22,20.5 23,21 L31,25 L31,28 L30,28 C26,28 19,24.5 15,22.5 Z";
+		String mountains = foreMountain + backMountain;
 
-		String pa = "11";
-		String plane = "M16,6 L28,12 L27,13 L16," + pa + " L5,13 L4,12 Z";
-		//String straightVortexes = "M6,14 L8.5,14 L11.5,18 L10,18 M26,14 L22,18 L20.5,18 L23.5,14 Z";
-		String straightVortexes = "M6,14 L8.5,13.6 L11.5,18 Z M26,14 L20.5,18 L23.5,13.6 Z";
-		String curvedVortexes = "M7.5,13.75 L10,16.5 A10.48875058760044,10.48875058760044,0,0,1,6,14 Z M24.5,13.75 L26,14 A10.48875058760044,10.48875058760044,0,0,1,22,16.5 Z";
-		String exhaust = "M12,13 L13.75,18 L14.75,18 L14,13 Z M18,13 L17.25,18 L18.25,18 L20,13 Z";
-		String wisps = curvedVortexes;
+		String vrr = "10.48875058760044,10.48875058760044";
+		String plane = "M16,6 L28,12 L27,13 L16,11 L5,13 L4,12 Z";
+		String wisps = "M7.5,13.75 L10,16.5 A" + vrr + ",0,0,1,6,14 Z M24.5,13.75 L26,14 A" + vrr + ",0,0,1,22,16.5 Z";
 
 		String sunMountainClip = "M0,0 L0,26.5 L9,22 C10,21.5 10.75,21.25 11.5,21.25 C12.25,21.25 13,21.5 14,22 L18,20 C19,19.5 19.75,19.25 20.5,19.25 C21.25,19.25 22,19.5 23,20  L32,24.5 L32,0 Z";
 
@@ -51,7 +49,7 @@ public class DesertSkyRangersIcon extends SvgIcon {
 		fill( sun, theme[ 0 ] );
 		clip( null );
 
-		fill( curvedMountain, theme[ 2 ] );
+		fill( mountains, theme[ 2 ] );
 		fill( plane, theme[ 1 ] );
 		fill( wisps, Colors.mix( theme[ 0 ], theme[ 1 ], 0.5 ) );
 	}
