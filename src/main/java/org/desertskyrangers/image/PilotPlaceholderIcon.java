91f38939be01ba7b89f41330d23305d561bd4ed8
msoderquist <msoderquist@mastercontrol.com>
2022-01-26 12:50:32 -0700

    Enhanced SvgIcon capability

--- src/main/java/org/desertskyrangers/image/PilotPlaceholderIcon.java
+++ src/main/java/org/desertskyrangers/image/PilotPlaceholderIcon.java
@@ -7,13 +7,9 @@ import java.nio.file.Paths;
 
 public class PilotPlaceholderIcon extends SvgIcon {
 
-	@Override
-	protected void doRender() {
-		super.doRender();
-
+	public PilotPlaceholderIcon() {
 		String head = circle( 16, 10, 6 );
 		String body = "M4,28 A12,10,0,0,1,28,28 Z";
-
 		fill( head );
 		fill( body );
 	}
@@ -24,7 +20,7 @@ public class PilotPlaceholderIcon extends SvgIcon {
 		PilotPlaceholderIcon icon = new PilotPlaceholderIcon().resize( 128 );
 
 		try {
-		new VectorImageWriter().save( icon, Paths.get( System.getProperty( "user.dir" ), "public", "pilot.png" ) );
+			new VectorImageWriter().save( icon, Paths.get( System.getProperty( "user.dir" ), "../../websites/www.desertskyrangers.org/public", "pilot.png" ) );
 		} catch( Exception exception ) {
 			exception.printStackTrace();
 		}
