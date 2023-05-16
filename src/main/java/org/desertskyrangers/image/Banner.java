ce5e360959f7460a677dfb3457d88685f00606bc
msoderquist <msoderquist@mastercontrol.com>
2023-05-16 14:42:50 -0600

    Add subtitle

--- src/main/java/org/desertskyrangers/image/Banner.java
+++ src/main/java/org/desertskyrangers/image/Banner.java
@@ -3,10 +3,12 @@ package org.desertskyrangers.image;
 import com.avereon.zarra.image.SvgIcon;
 import com.avereon.zarra.image.VectorImageWriter;
 import javafx.application.Platform;
+import javafx.print.Paper;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.transform.Affine;
 
+import java.lang.reflect.Constructor;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -28,12 +30,16 @@ public class Banner extends SvgIcon {
 		draw( new DesertSkyRangersIcon() );
 
 		// DSR text
-		transform( Affine.translate( 34, 14 ).createConcatenation( Affine.scale( 0.7, 0.7 ) ) );
+		transform( Affine.translate( 34, 10 ).createConcatenation( Affine.scale( 0.7, 0.7 ) ) );
 		setFont( Font.font(getFont().getName(), FontWeight.BOLD, getFont().getSize()) );
 		text( "Desert Sky Rangers", DesertSkyRangersIcon.THEME[ 1 ] );
 
 		setFont( Font.font(getFont().getName(), FontWeight.NORMAL, getFont().getSize()) );
-		transform( Affine.translate( 42, 26 ).createConcatenation( Affine.scale( 0.5,0.5 ) ) );
+		transform( Affine.translate( 56, 16 ).createConcatenation( Affine.scale( 0.4,0.4 ) ) );
+		text( "UAV Flying Club", DesertSkyRangersIcon.THEME[ 1 ] );
+
+		setFont( Font.font(getFont().getName(), FontWeight.NORMAL, getFont().getSize()) );
+		transform( Affine.translate( 42, 28 ).createConcatenation( Affine.scale( 0.5,0.5 ) ) );
 		text( "desertskyrangers.org", DesertSkyRangersIcon.THEME[ 1 ] );
 	}
 
