ba0246f789e3945939d631ec5b69cbb965bec14e
Mark Soderquist <mark@soderquist.net>
2024-10-06 12:04:31 -0600

    Update build configuration

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,7 @@
 module org.desertskyrangers.image {
 	requires com.avereon.zarra;
 	requires javafx.controls;
-	requires javafx.graphics;
 	requires javafx.swing;
-	requires image4j;
 
 	exports org.desertskyrangers.image;
 }
